96e81562fb4b0017cb09e10aecaf668cc453f94d
gzpeterz <zhangwen@gzpeter.com>
2017-10-31 10:20:43 +0800

    before test maven

--- src/main/java/com/hc/mybatis/po/Student.java
+++ src/main/java/com/hc/mybatis/po/Student.java
@@ -60,10 +60,4 @@ public class Student {
     public void setSex(Integer sex) {
         this.sex = sex;
     }
-
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", chinese=" + chinese + ", english=" + english + ", math="
-				+ math + "]";
-	}
 }
